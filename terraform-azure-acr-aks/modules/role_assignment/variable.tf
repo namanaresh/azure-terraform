8c93c7e7ef1a7e3b554dca3095c02226be3ac021
Naresh <139313434+namanaresh@users.noreply.github.com>
2026-07-11 18:24:17 +0530

    Update variable.tf

--- terraform-azure-acr-aks/modules/role_assignment/variable.tf
+++ terraform-azure-acr-aks/modules/role_assignment/variable.tf
@@ -7,3 +7,7 @@ variable "acr_id" {
   description = "The resource ID of the Azure Container Registry"
   type        = string
 }
+variable "role_definition_name" {
+  description = "The resource ID of the Azure Container Registry"
+  type        = string
+}
