7a1b693bb2f1a823d546607f52ae8f638ed12be3
Naresh <139313434+namanaresh@users.noreply.github.com>
2026-07-11 18:22:29 +0530

    Update main.tf

--- terraform-azure-acr-aks/modules/role_assignment/main.tf
+++ terraform-azure-acr-aks/modules/role_assignment/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "aks_acr" {
   principal_id         = var.principal_id
-  role_definition_name = "AcrPull"
+  role_definition_name = var.role_definition_name
   scope                = var.acr_id
 }
