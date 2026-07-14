27416a7ea9439a966490ca6568627da8ae8095d1
Naresh <139313434+namanaresh@users.noreply.github.com>
2026-07-14 11:52:13 +0530

    Update variable.tf

--- terraform-azure-acr-aks/variable.tf
+++ terraform-azure-acr-aks/variable.tf
@@ -1,1 +1,32 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
 
+variable "location" {
+  description = "The resource group location"
+  type        = string
+}
+
+variable "acr_name" {
+  description = "The resource group location"
+  type        = string
+}
+variable "dns_prefix" {
+  description = "dns_prefix"
+  type        = string
+}
+
+variable "node_count" {
+  description = "node_count"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "vm_size"
+  type        = string
+}
+variable "aks_name" {
+  description = "aks_name"
+  type        = string
+}
