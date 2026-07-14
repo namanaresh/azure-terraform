97f15409fa0148d282ca214a8007a9ccb2814723
Naresh <139313434+namanaresh@users.noreply.github.com>
2026-07-14 11:51:33 +0530

    Update main.tf

--- terraform-azure-acr-aks/main.tf
+++ terraform-azure-acr-aks/main.tf
@@ -1,11 +1,20 @@
-resource "azurerm_resource_group" "rg" {
+module "resource_group" {
+  source   = "./modules/resource_group"
   name     = var.resource_group_name
   location = var.location
 }
-
 module "acr" {
-  source              = "./modules/acr"
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  source   = "./modules/acr"
+  name     = var.acr_name
+  resource_group_name = module.resource_group.name
+  location = module.resource_group.location
+}
+module "aks" {
+  source   = "./modules/aks"
+  name     = var.aks_name
+  resource_group_name = module.resource_group.name
+  location = module.resource_group.location
+  dns_prefix          = var.dns_prefix
+  node_count          = var.node_count
+  vm_size             = var.vm_size
 }
